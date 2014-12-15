097ff05c048c07fcd0d7409db1bacaeb7edf5466
ck49 <coolking49@gmail.com>
2014-12-14 22:43:17 -0600

    Create PokemonInterface.java

---
+++ PokemonInterface.java
@@ -0,0 +1,25 @@
+public interface PokemonInterface{
+
+public int get_price();
+
+public boolean isCarnivore();
+
+public int prey_rank();
+
+public int catch();
+
+public int rarity();
+
+public int population();
+
+public boolean mountain();
+
+public boolean lake();
+
+public boolean grass();
+
+public void set_population(int new_pop);
+
+public String description();
+
+}
