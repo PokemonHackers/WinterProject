3d14d1cd6938b6b5db94fa2c36a0646020b5de71
ganhari123 <ganapathy.hari.narayan@gmail.com>
2014-12-17 10:00:46 +0530

    Create Driver.java
    Version 1.0

---
+++ Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+    public static void main(String[] args) {
+        Pokemon k = new Kingdra();
+        Pokemon m = new Magikarp();
+        System.out.println(k.toString() + "\n" + m.toString());
+    }
+}
