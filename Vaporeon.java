849f6d8f1ae95351c75cfd204736b16b08593221
ganhari123 <ganapathy.hari.narayan@gmail.com>
2014-12-17 10:13:15 +0530

    Update Vaporeon.java
    Version 1.1

--- Vaporeon.java
+++ Vaporeon.java
@@ -1,64 +1,19 @@
-public class Vaporeon implements PokemonInterface{
+public class Vaporean extends Pokemon{
 
-private boolean carnivore = true;
-private int prey_rank = 3;
-private int hatch = 36;
-private int catch_rate = 45;
-private int rarity = 8;
-private int population = 0;
-private boolean mountain = false;
-private boolean lake = true;
-private boolean grass = false;
-private int price = 800;
-private String description = "These friendly creatures are a popular attraction - rare, strong, and playful. However, they aren't easy to take of."
+    public Vaporean(){
+        carnivore = true;
+        prey_rank = 3;
+        hatch = 36;
+        catch_rate = 45;
+        rarity = 8;
+        population = 0;
+        mountain = false;
+        lake = true;
+        grass = false;
+        price = 800;
+    } // change this to the pokemon name
 
-public Vaporeon(){} // change this to the pokemon name
-
-public String description(){
-	return this.description;
-}
-
-public int get_price(){
-	return this.price;
-}
-
-public boolean isCarnivore(){
-	return this.carnivore;
-}
-
-public int prey_rank(){
-	return this.prey_rank;
-}
-
-public int catch(){
-	return this.catch_rate;
-}
-
-public int rarity(){
-	return this.rarity;
-}
-
-public int population(){
-	return this.population;
-}
-
-public boolean mountain(){
-	return this.mountain;
-}
-
-public boolean lake(){
-	return this.lake;
-}
-public boolean grass(){
-	return this.grass;
-}
-
-public void set_population(int new_pop){
-	this.population = new_pop;
-}
-
-
-
-
-
+    public String toString(){
+        return "These friendly creatures are a popular attraction - rare, strong, and playful. However, they aren't easy to take of.";
+    }
 }
